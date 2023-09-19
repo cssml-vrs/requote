e193ac51305e365acc98912614663c1865314956
cssml-vrs <matthew.liew@yahoo.com>
2023-09-20 01:48:58 +0800

    background img function WIP

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -14,7 +14,7 @@ const Quotes = () => {
     
     
     let url = `https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`;
-    fetch('react-quotes-app-data')
+    fetch(url)
       .then(res => res.json())
       .then(data => {
         let dataQuotes = data.quotes;
--- src/routes/Settings.js
+++ src/routes/Settings.js
@@ -1,7 +1,24 @@
 const Settings = () => {
+
+  const image_input = document.querySelector("#image_input");
+var uploaded_image = "";
+
+image_input.addEventListener("change", function() {
+  console.log(image_input.value);
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    uploaded_image = reader.result;
+    document.querySelector("#display_image").computedStyleMap.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+})
   return (
     <div className="bg page2">
       <h1>Settings</h1>
+      <p1>Upload background image</p1>
+      <input type="file" id="image_input" accept="image/png, image/jpg"></input>
+      <div id="display_image"></div>
+      <script src="background.js"></script>
     </div>
   );
 };
